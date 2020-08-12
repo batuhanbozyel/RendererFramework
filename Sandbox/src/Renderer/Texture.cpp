8e16ebe638874f56ce9a37430313297e33e91bbe
batuhanbozyel <batuhanbozyel@users.noreply.github.com>
2020-08-12 22:25:41 +0300

    Added caching to Texture class

--- Sandbox/src/Renderer/Texture.cpp
+++ Sandbox/src/Renderer/Texture.cpp
@@ -26,7 +26,8 @@ Texture::Texture()
 	m_Handles[m_Count] = glGetTextureHandleARB(m_IDs[m_Count]);
 	glMakeTextureHandleResidentARB(m_Handles[m_Count]);
 	glBindTexture(GL_TEXTURE_2D, 0);
-	m_Count++;
+
+	m_TextureMap.insert(std::make_pair(std::string(), m_Count++));
 }
 
 Texture::~Texture()
@@ -40,33 +41,39 @@ Texture::~Texture()
 
 const uint64_t Texture::LoadTexture(const char* path)
 {
-	glBindTexture(GL_TEXTURE_2D, m_IDs[m_Count]);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	stbi_set_flip_vertically_on_load(1);
-	int width, height, channels;
-	unsigned char* buffer = stbi_load(path, &width, &height, &channels, 4);
-	if (buffer)
+	auto& textureIt = m_TextureMap.find(path);
+	if (textureIt == m_TextureMap.end())
 	{
-		if (m_Count < max_textures)
+		glBindTexture(GL_TEXTURE_2D, m_IDs[m_Count]);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+		stbi_set_flip_vertically_on_load(1);
+		int width, height, channels;
+		unsigned char* buffer = stbi_load(path, &width, &height, &channels, 4);
+		if (buffer)
 		{
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-			m_Handles[m_Count] = glGetTextureHandleARB(m_IDs[m_Count]);
-			glMakeTextureHandleResidentARB(m_Handles[m_Count]);
-			glBindTexture(GL_TEXTURE_2D, 0);
-			uint64_t handle = m_Handles[m_Count];
-			m_Count++;
+			if (m_Count < max_textures)
+			{
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+				m_Handles[m_Count] = glGetTextureHandleARB(m_IDs[m_Count]);
+				glMakeTextureHandleResidentARB(m_Handles[m_Count]);
+				glBindTexture(GL_TEXTURE_2D, 0);
+				uint64_t handle = m_Handles[m_Count];
+				m_TextureMap.insert(std::make_pair(path, m_Count++));
+				stbi_image_free(buffer);
+				return handle;
+			}
+			LOG_WARN("Texture slots are occupied!");
 			stbi_image_free(buffer);
-			return handle;
 		}
-		LOG_WARN("Texture slots are occupied!");
-		stbi_image_free(buffer);
+		LOG_ERROR("Could not load texture!");
+		return DefaultTextureFromMap();
 	}
-	LOG_ERROR("Could not load texture!");
-	return 0;
+	
+	return m_Handles[textureIt->second];
 }
